17b9738bb039d49c357ad803ea4028d262fdd847
Robin <robins7.wiki@gmail.com>
2022-09-26 22:18:16 +0200

    Alpha 0.1.0 (.exe + installer) – základní funkčnosti (postava, bomby, životy, cíl, čas, skóre); funkční, jednorázové

--- functions.py
+++ functions.py
@@ -1,10 +1,22 @@
-import random
+import math, random
 
 def time_to_minutes(time):
     minutes = str(time//60)
     seconds = time % 60
     seconds = f"{seconds:02d}"
-    output = minutes+":"+seconds
+    return minutes+":"+seconds
+
+def random_pos(maxwidth, maxheight, char_pos=(-1000, -1000), targ_pos=(-1000, 1000)):
+    output = (random.randint(32, maxwidth-32), random.randint(132, maxheight-32))
+    while (math.isclose(output[0], char_pos[0], abs_tol=70) or math.isclose(output[1], char_pos[1], abs_tol=70)) or (math.isclose(output[0], targ_pos[0], abs_tol=70) or math.isclose(output[1], targ_pos[1], abs_tol=70)):
+        output = (random.randint(32, maxwidth-32), random.randint(132, maxheight-32))
     return output
 
-# def random_target_pos(maxwidth, maxheight)
+def minusHeart(hearts):
+    if hearts[2]:
+        hearts[2] = False
+    elif hearts[1]:
+        hearts[1] = False
+    elif hearts[0]:
+        hearts[0] = False
+    return hearts
--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-import pygame, random, functions
+import pygame, math, random, functions
 from functions import *
 
 #### Inicializace
 pygame.init()
 screen_sizes = (1200, 650)
 screen = pygame.display.set_mode(screen_sizes)
-pygame.display.set_caption("Hunting Game")
+pygame.display.set_caption("Hunting Game (Alpha 0.1.0)")
 favicon = pygame.image.load("images/favicon.png")
 pygame.display.set_icon(favicon)
 
 #### Definování
 ### In-game
 score = 0
-goal = 15
+goal = 25
 lives = 3
-heart1 = True
-heart2 = True
-heart3 = True
-time = 120
+hearts = [True, True, True]
+time = 90
+win = False
+message = ""
 
-# target_pos = (random.randint(32, screen_sizes[0]-32), random.randint(89, screen_sizes[1]-32))
-# bomb1_pos = (random.randint(32, screen_sizes[0]-32), random.randint(89, screen_sizes[1]-32))
-# bomb1_pos = (random.randint(32, screen_sizes[0]-32), random.randint(89, screen_sizes[1]-32))
-# bomb1_pos = (random.randint(32, screen_sizes[0]-32), random.randint(89, screen_sizes[1]-32))
+character_spawn = (screen_sizes[0]//2, screen_sizes[1]//2)
+target_pos = random_pos(screen_sizes[0], screen_sizes[1], character_spawn)
+bomb1_pos = random_pos(screen_sizes[0], screen_sizes[1], character_spawn, target_pos)
+bomb2_pos = random_pos(screen_sizes[0], screen_sizes[1], character_spawn, target_pos)
+bomb3_pos = random_pos(screen_sizes[0], screen_sizes[1], character_spawn, target_pos)
 
 ### Technické
 ## Čas, délky a velikosti
 fps = 60
+time_counter = 0
 clock = pygame.time.Clock()
 
 ## Barvy
@@ -34,6 +36,8 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 gray = (130, 130, 130)
 light_green = pygame.Color("#90ee90")
+light_green2 = pygame.Color("#94ffbf")
+light_red = pygame.Color("#ffa1b6")
 cream = pygame.Color("#fcf5ca")
 gold = pygame.Color("#ffd700")
 background = pygame.Color("#cfccb5")
@@ -46,7 +50,7 @@ favicon_medium_rect.topleft = (10, 6)
 
 character_image = pygame.image.load("images/character.png")
 character_image_rect = character_image.get_rect()
-character_image_rect.center = (screen_sizes[0]//2, screen_sizes[1]//2)
+character_image_rect.center = character_spawn
 
 target_image = pygame.image.load("images/target.png")
 target_image_rect = target_image.get_rect()
@@ -54,12 +58,15 @@ target_image_rect.center = target_pos
 
 bomb1_image = pygame.image.load("images/bomb.png")
 bomb1_image_rect = character_image.get_rect()
+bomb1_image_rect.center = bomb1_pos
 
 bomb2_image = pygame.image.load("images/bomb.png")
 bomb2_image_rect = character_image.get_rect()
+bomb2_image_rect.center = bomb2_pos
 
 bomb3_image = pygame.image.load("images/bomb.png")
 bomb3_image_rect = character_image.get_rect()
+bomb3_image_rect.center = bomb3_pos
 
 npc_image = pygame.image.load("images/npc.png")
 npc_image_rect = character_image.get_rect()
@@ -114,7 +121,7 @@ npc_sound.set_volume(.3)
 ## Písmo
 # fonty
 heading_font = pygame.font.SysFont("arial", 38, True)
-bar_font = pygame.font.SysFont("kokila", 30)
+final_font = pygame.font.SysFont("kokila", 64, True)
 
 # texty
 game_name_text = heading_font.render("Hunting Game", True, gold)
@@ -141,7 +148,7 @@ while lets_continue:
 
 
     ## Mimo event
-    if pygame.key.get_pressed()[pygame.K_UP] and character_image_rect.top > 84:
+    if pygame.key.get_pressed()[pygame.K_UP] and character_image_rect.top > 100:
         character_image_rect.y -= 5
     elif pygame.key.get_pressed()[pygame.K_DOWN] and character_image_rect.bottom < screen_sizes[1]:
         character_image_rect.y += 5
@@ -154,9 +161,42 @@ while lets_continue:
         score += 1
         collect_sound.play()
         if score == goal:
+            win = True
+            message = "Vyhráli jste!"
             break
-        target_pos = (random.randint(32, screen_sizes[0]-32), random.randint(89, screen_sizes[1]-32))
+        target_pos = random_pos(screen_sizes[0], screen_sizes[1], character_image_rect.center)
+        bomb1_pos = random_pos(screen_sizes[0], screen_sizes[1], character_image_rect.center, target_pos)
+        bomb2_pos = random_pos(screen_sizes[0], screen_sizes[1], character_image_rect.center, target_pos)
+        bomb3_pos = random_pos(screen_sizes[0], screen_sizes[1], character_image_rect.center, target_pos)
         target_image_rect.center = target_pos
+        bomb1_image_rect.center = bomb1_pos
+        bomb2_image_rect.center = bomb2_pos
+        bomb3_image_rect.center = bomb3_pos
+    
+    if character_image_rect.colliderect(bomb1_image_rect):
+        boom_sound.play()
+        hit_sound.play()
+        bomb1_pos = random_pos(screen_sizes[0], screen_sizes[1], character_image_rect.center, target_pos)
+        bomb1_image_rect.center = bomb1_pos
+        hearts = minusHeart(hearts)
+
+    if character_image_rect.colliderect(bomb2_image_rect):
+        boom_sound.play()
+        hit_sound.play()
+        bomb2_pos = random_pos(screen_sizes[0], screen_sizes[1], character_image_rect.center, target_pos)
+        bomb2_image_rect.center = bomb2_pos
+        hearts = minusHeart(hearts)
+
+    if character_image_rect.colliderect(bomb3_image_rect):
+        boom_sound.play()
+        hit_sound.play()
+        bomb3_pos = random_pos(screen_sizes[0], screen_sizes[1], character_image_rect.center, target_pos)
+        bomb3_image_rect.center = bomb3_pos
+        hearts = minusHeart(hearts)
+
+    if not hearts[0]:
+        message = "Umřeli jste!"
+        break
 
     ### Periodické renderování
     screen.fill(background)
@@ -164,18 +204,20 @@ while lets_continue:
     pygame.draw.rect(screen, cream, (0, 55, screen_sizes[0], 45))
     screen.blit(favicon_medium, favicon_medium_rect)
     screen.blit(game_name_text, game_name_text_rect)
+    timer_text = heading_font.render(f"Timer: {time_to_minutes(time)}", True, gold)
+    score_text = heading_font.render(f"Score: {score}", True, gold)
     screen.blit(timer_text, timer_text_rect)
     screen.blit(score_text, score_text_rect)
 
-    if heart1:
+    if hearts[0]:
         screen.blit(heart1_image, heart1_image_rect)
     else:
         screen.blit(heart_empty1_image, heart_empty1_image_rect)
-    if heart2:
+    if hearts[1]:
         screen.blit(heart2_image, heart2_image_rect)
     else:
         screen.blit(heart_empty2_image, heart_empty2_image_rect)
-    if heart3:
+    if hearts[2]:
         screen.blit(heart3_image, heart3_image_rect)
     else:
         screen.blit(heart_empty3_image, heart_empty3_image_rect)
@@ -183,18 +225,68 @@ while lets_continue:
     pygame.draw.rect(screen, gray, (350, 68, 800, 20))
     pygame.draw.rect(screen, light_green, (360, 73, 780, 10))
 
+    screen.blit(bomb1_image, bomb1_image_rect)
+    screen.blit(bomb2_image, bomb2_image_rect)
+    screen.blit(bomb3_image, bomb3_image_rect)
+    screen.blit(target_image, target_image_rect)
+    screen.blit(character_image, character_image_rect)
 
     ## Update
     pygame.display.update()
+    time_counter += 1
+    if time_counter == fps:
+        time_counter = 0
+        time -= 1
+        if time == 0:
+            message = "Čas vypršel!"
+            break
     clock.tick(fps)
 
 ## Konec hry
-if not lets_continue:
-    pygame.quit()
-else:
+if lets_continue:
     pygame.mixer.music.stop()
-    win_sound.play()
     screen.fill(background)
+    pygame.draw.rect(screen, gray, (0, 0, screen_sizes[0], 70))
+    pygame.draw.rect(screen, cream, (0, 55, screen_sizes[0], 45))
+    screen.blit(favicon_medium, favicon_medium_rect)
+    screen.blit(game_name_text, game_name_text_rect)
+    timer_text = heading_font.render(f"Timer: {time_to_minutes(time)}", True, gold)
+    score_text = heading_font.render(f"Score: {score}", True, gold)
+    screen.blit(timer_text, timer_text_rect)
+    screen.blit(score_text, score_text_rect)
+
+    if hearts[0]:
+        screen.blit(heart1_image, heart1_image_rect)
+    else:
+        screen.blit(heart_empty1_image, heart_empty1_image_rect)
+    if hearts[1]:
+        screen.blit(heart2_image, heart2_image_rect)
+    else:
+        screen.blit(heart_empty2_image, heart_empty2_image_rect)
+    if hearts[2]:
+        screen.blit(heart3_image, heart3_image_rect)
+    else:
+        screen.blit(heart_empty3_image, heart_empty3_image_rect)
+    
+    pygame.draw.rect(screen, gray, (350, 68, 800, 20))
+    pygame.draw.rect(screen, light_green, (360, 73, 780, 10))
+
+    screen.blit(bomb1_image, bomb1_image_rect)
+    screen.blit(bomb2_image, bomb2_image_rect)
+    screen.blit(bomb3_image, bomb3_image_rect)
+    screen.blit(target_image, target_image_rect)
+    if win:
+        win_sound.play()
+        pygame.draw.rect(screen, gray, (screen_sizes[0]//2-396, screen_sizes[1]//2-121, 800, 250))
+        pygame.draw.rect(screen, light_green2, (screen_sizes[0]//2-400, screen_sizes[1]//2-125, 800, 250))
+    else:
+        loose_sound.play()
+        pygame.draw.rect(screen, gray, (screen_sizes[0]//2-396, screen_sizes[1]//2-121, 800, 250))
+        pygame.draw.rect(screen, light_red, (screen_sizes[0]//2-400, screen_sizes[1]//2-125, 800, 250))
+    final_text = final_font.render(message, False, black)
+    final_text_rect = final_text.get_rect()
+    final_text_rect.center = (screen_sizes[0]//2, screen_sizes[1]//2)
+    screen.blit(final_text, final_text_rect)
     
     while lets_continue:
         pygame.display.update()
